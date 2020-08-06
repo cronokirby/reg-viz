f44159cb2bab333d0b60d2af78f7b01a271d6f9a
Lúcás Meier <cronokirby@gmail.com>
2020-08-06 16:52:19 +0200

    Add an example property test
    This is just an example to show how to integrate property tests, once
    again with trivial examples.
    
    The next step is to add actual tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,32 @@
+{-# LANGUAGE BlockArguments #-}
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
 import Test.Hspec
 import Prelude
 
 main :: IO ()
-main = void $ traverse hspec specs
+main = do
+  traverse hspec specs
+  traverse checkParallel groups |> void
 
 specs :: [SpecWith ()]
 specs = [exampleSpec]
 
+groups :: [Group]
+groups = [exampleGroup]
+
 exampleSpec :: SpecWith ()
 exampleSpec =
-  describe "Example" <| do
-    it "is just an example test" <| do
+  describe "Example" do
+    it "is just an example test" do
       1 `shouldBe` 1
       2 + 2 `shouldBe` 4
+
+exampleGroup :: Group
+exampleGroup = Group "Example Group" [("example property", exampleProperty)]
+
+exampleProperty :: Property
+exampleProperty = property do
+  b <- forAll Gen.bool
+  b === b
