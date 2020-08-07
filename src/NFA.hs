c5b3910372dede0695dd6c4e2429d761c9a601cc
Lúcás Meier <cronokirby@gmail.com>
2020-08-07 12:54:46 +0200

    Allow converting regexes to NFAs

--- src/NFA.hs
+++ src/NFA.hs
@@ -1,7 +1,8 @@
-module NFA (NFA, Transition, only, sequenced, WriterM) where
+module NFA (NFA, fromRegex, writeNFA) where
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
+import Regex (Regex (..))
 import Prelude
 
 -- Represents a label we can have on an arrow of an NFA
@@ -22,12 +23,22 @@ showTransition f (WithSymbols (c :| cs)) = foldr (\x acc -> f x <> ", " <> acc) 
 -- as well as a set of all states, a set of accept states, and a map from each state to its outgoing transitions
 data NFA s a = NFA a (Set a) (Set a) (Map a [(Transition s, a)]) deriving (Eq, Show)
 
+simplifyStates :: Ord a => NFA s a -> NFA s Integer
+simplifyStates (NFA start states accept trans) =
+  NFA (f start) (Set.map f states) (Set.map f accept) (Map.mapKeys f (fmap (\(t, a) -> (t, f a)) <$> trans))
+  where
+    stateMap = Map.fromList (zip (Set.toList states) [1 ..])
+    f s = Map.findWithDefault 0 s stateMap
+
+emptyNFA :: NFA s Integer
+emptyNFA = NFA 0 (Set.empty) (Set.empty) (Map.empty)
+
 -- Create an NFA for a regex only recognizing a string with a single symbol
-only :: s -> NFA s Int
+only :: s -> NFA s Integer
 only s = NFA 0 (fromList [0, 1, 2]) (fromList [1]) (fromList [(0, [(WithSymbols (one s), 1)]), (1, [(Rest, 2)])])
 
 -- An NFA represented the language brought about by composition
-sequenced :: (Ord a, Ord b) => NFA s a -> NFA s b -> NFA s (Either a b)
+sequenced :: (Ord a, Ord b) => NFA s a -> NFA s b -> NFA s Integer
 sequenced (NFA start1 states1 accept1 trans1) (NFA start2 states2 accept2 trans2) =
   let start = Left start1
       states = Set.union (Set.map Left states1) (Set.map Right states2)
@@ -36,14 +47,11 @@ sequenced (NFA start1 states1 accept1 trans1) (NFA start2 states2 accept2 trans2
       bothTransitions = Map.union (changeMap Left trans1) (changeMap Right trans2)
       extraTransitions = Map.fromList (zip (Left <$> Set.toList accept1) (repeat [(Empty, Right start2)]))
       transitions = Map.unionWith (++) extraTransitions bothTransitions
-   in NFA start states accept transitions
+   in simplifyStates (NFA start states accept transitions)
 
-simplifyStates :: Ord a => NFA s a -> NFA s Integer
-simplifyStates (NFA start states accept trans) =
-  NFA (f start) (Set.map f states) (Set.map f accept) (Map.mapKeys f (fmap (\(t, a) -> (t, f a)) <$> trans))
-  where
-    stateMap = Map.fromList (zip (Set.toList states) [1 ..])
-    f s = Map.findWithDefault 0 s stateMap
+fromRegex :: Regex s -> NFA s Integer
+fromRegex (Only s) = only s
+fromRegex (Sequenced rs) = rs |> fmap fromRegex |> foldr sequenced emptyNFA
 
 -- A class for being able to write things
 class Monad m => WriterM m where
@@ -73,6 +81,11 @@ writeNFA f nfa = do
     write (show i)
     write "\n"
   write "}\n"
+  write "strt [style=invis]\n"
+  write "strt ->"
+  write "s"
+  write (show start)
+  write "\n"
   forM_ (edges trans) <| \(a, a', t) -> do
     write "s"
     write (show a)
